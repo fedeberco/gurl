582741d0cccd5987974f4c00e76c8f3448cfd789
fedeberco <berco.fede@gmail.com>
2020-11-30 16:36:06 -0300

    fix description

--- cmd/call/call.go
+++ cmd/call/call.go
@@ -46,7 +46,7 @@ func init() {
 	ConfigureFlags(flags)
 }
 
-// Configure flags not only in this project but for those projects that import this one
+// ConfigureFlags registers the flags not only in this project but for those projects that import this one
 func ConfigureFlags(flags *pflag.FlagSet){
 	flags.StringVarP(&uri, "uri", "u", "", "gRPC URI in the form of host:port/service_name/method_name")
 	flags.StringVarP(&data, "data", "d", "", "Data, as JSON string, to send to the gRPC service")
